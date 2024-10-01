5be383465bc22a84c4960dae304584315c7fa732
Josep Vicent Mestre Llobell <jmestrellobell@gmail.com>
2024-10-02 01:22:38 +0200

    Ejercicio4 terminado

---
+++ src/main/java/com/byjosep04/tema03/Ejercicio4.java
@@ -0,0 +1,23 @@
+package com.byjosep04.tema03;
+
+import java.util.Scanner;
+
+public class Ejercicio4 {
+    public static void main(String[] args) {
+        double primerNumero, segundoNumero;
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Ingrese el primer numero");
+        primerNumero = Double.parseDouble(scanner.nextLine());
+        System.out.println("Ingrese el segundo numero");
+        segundoNumero = Double.parseDouble(scanner.nextLine());
+
+        scanner.close();
+
+        if (primerNumero < segundoNumero) {
+            System.out.println("El orden es "+ segundoNumero+", "+ primerNumero);
+        }else if (primerNumero >= segundoNumero) {
+            System.out.println("El orden es "+ primerNumero+", "+segundoNumero);
+        }
+
+    }
+}
