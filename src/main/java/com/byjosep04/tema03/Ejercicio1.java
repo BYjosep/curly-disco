c0a6adecd0fa24ef6dea129bcbe452c9e1282cb3
Josep Vicent Mestre Llobell <jmestrellobell@gmail.com>
2024-10-01 16:27:56 +0200

    Ejercicio1 terminado

---
+++ src/main/java/com/byjosep04/tema03/Ejercicio1.java
@@ -0,0 +1,18 @@
+package com.byjosep04.tema03;
+
+import java.util.Scanner;
+
+public class Ejercicio1 {
+    public static void main(String[] args) {
+    float numeroIntroduciodo;
+        Scanner scaner = new Scanner(System.in);
+        System.out.println("Escriba un numero");
+        numeroIntroduciodo = Float.parseFloat(scaner.nextLine());
+        scaner.close();
+        if (numeroIntroduciodo % 2 == 0) {
+            System.out.println("El numero "+numeroIntroduciodo+" es par");
+        }else {
+            System.out.println("El numero "+numeroIntroduciodo+" es impar");
+        }
+    }
+}
--- src/main/java/com/byjosep04/tema03/ejercicio1.java
+++
@@ -1,7 +0,0 @@
-package com.byjosep04.tema03;
-
-public class ejercicio1 {
-    public static void main(String[] args) {
-        System.out.println("Ejercicio1");
-    }
-}
