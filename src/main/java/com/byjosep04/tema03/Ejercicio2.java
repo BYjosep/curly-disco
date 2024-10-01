c2e5b106463b058e8fd5b7a6f14c3ea961c78739
Josep Vicent Mestre Llobell <jmestrellobell@gmail.com>
2024-10-01 16:51:28 +0200

    Ejercicio2 terminado

---
+++ src/main/java/com/byjosep04/tema03/Ejercicio2.java
@@ -0,0 +1,24 @@
+package com.byjosep04.tema03;
+
+import java.util.Scanner;
+
+public class Ejercicio2 {
+    public static void main(String[] args) {
+        float primerNumero, segundoNumero;
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Indique el primer numero");
+        primerNumero = Float.parseFloat(scanner.nextLine());
+        System.out.println("Indique el segundo numero");
+        segundoNumero = Float.parseFloat(scanner.nextLine());
+        scanner.close();
+        if (primerNumero == segundoNumero){
+            System.out.println("El primer numero es igual que el segundo");
+        } else if (primerNumero<segundoNumero){
+            System.out.println("El primer numero es menor que el segundo");
+        }else if (primerNumero>segundoNumero){
+            System.out.println("El primer numero es mayor que el segundo");
+        }
+
+    }
+
+}
