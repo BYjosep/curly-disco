3b6db625692db3b63cb96d5cfb5ce8b975704f33
Josep Vicent Mestre Llobell <jmestrellobell@gmail.com>
2024-10-02 02:38:00 +0200

    Ejercicio6 Falta crear la estructura de monedas y todo el resto

---
+++ src/main/java/com/byjosep04/tema03/Ejercicio6.java
@@ -0,0 +1,14 @@
+package com.byjosep04.tema03;
+
+import java.util.Scanner;
+
+public class Ejercicio6 {
+    public static void main(String[] args) {
+        float dinero;
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Inserte el dinero");
+        dinero = Float.parseFloat(scanner.nextLine());
+        scanner.close();
+
+    }
+}
