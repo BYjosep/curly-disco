efdc4b387e89b7a99b9ff27b06cb09e7e56c44e9
Josep Vicent Mestre Llobell <jmestrellobell@gmail.com>
2024-10-02 10:47:00 +0200

    Ejercicio6 Terminado

--- src/main/java/com/byjosep04/tema03/Ejercicio6.java
+++ src/main/java/com/byjosep04/tema03/Ejercicio6.java
@@ -4,10 +4,47 @@ import java.util.Scanner;
 
 public class Ejercicio6 {
     public static void main(String[] args) {
-        float dinero;
+        double dineroDecimal;
+        int dinero;
         Scanner scanner = new Scanner(System.in);
         System.out.println("Inserte el dinero");
-        dinero = Float.parseFloat(scanner.nextLine());
+        dineroDecimal = Double.parseDouble(scanner.nextLine());
+        dineroDecimal=dineroDecimal * 100;
+        dinero = (int) dineroDecimal;
+
+        if (dinero >= 200) {
+            System.out.println("Necesitas monedas de 2 euros: " + dinero / 200);
+            dinero %= 200;
+        }
+        if (dinero >= 100) {
+            System.out.println("Necesitas monedas de 1 euro: " + dinero / 100);
+            dinero %= 100;
+        }
+        if (dinero >= 50) {
+            System.out.println("Necesitas monedas de 50 centimos: " + dinero / 50);
+            dinero %= 50;
+        }
+        if (dinero >= 20) {
+            System.out.println("Necesitas monedas de 20 centimos: " + dinero / 20);
+            dinero %= 20;
+        }
+        if (dinero >= 10) {
+            System.out.println("Necesitas monedas de 10 centimos: "+ (dinero / 10));
+            dinero %= 10;
+        }
+        if (dinero >= 5) {
+            System.out.println("Necesitas monedas de 5 centimos: " + dinero / 5);
+            dinero %= 5;
+        }
+        if (dinero >= 2) {
+            System.out.println("Necesitas monedas de 2 centimos: " + dinero / 2);
+            dinero %= 2;
+        }
+        if (dinero >= 1) {
+            System.out.println("Necesitas monedas de 1 centimo: " + dinero);
+            dinero = 0;
+        }
+
         scanner.close();
 
     }
