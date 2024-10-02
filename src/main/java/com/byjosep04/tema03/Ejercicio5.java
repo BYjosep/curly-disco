b2df6541d69a92b326b79dfeb80c6566757d85ec
Josep Vicent Mestre Llobell <jmestrellobell@gmail.com>
2024-10-02 02:22:18 +0200

    Ejercicio5 terminado

---
+++ src/main/java/com/byjosep04/tema03/Ejercicio5.java
@@ -0,0 +1,31 @@
+package com.byjosep04.tema03;
+
+import java.util.Scanner;
+
+public class Ejercicio5 {
+    public static void main(String[] args) {
+        int  nota;
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Ingrese la nota en formato numerico");
+        nota = Integer.parseInt(scanner.nextLine());
+        scanner.close();
+        switch (nota) {
+            case 0: case 1: case 2: case 3: case 4:
+                System.out.println("INSUFICIENTE");
+                break;
+            case 5: case 6:
+                System.out.println("SUFICIENTE");
+                break;
+            case 7: case 8:
+                System.out.println("BIEN");
+                break;
+            case 9: case 10:
+                System.out.println("EXELENTE");
+                break;
+            default:
+                System.out.println("ERROR");
+                break;
+        }
+
+    }
+}
