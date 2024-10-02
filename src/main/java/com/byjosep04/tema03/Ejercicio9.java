d6e63db6a21f26e0621b3842a89ad7b1eda563df
Josep Vicent Mestre Llobell <jmestrellobell@gmail.com>
2024-10-03 01:13:52 +0200

    Ejercicio9 Terminado

--- src/main/java/com/byjosep04/tema03/Ejercicio9.java
+++ src/main/java/com/byjosep04/tema03/Ejercicio9.java
@@ -14,11 +14,14 @@ public class Ejercicio9 {
         System.out.println("Ingrese el lado 3");
         lado3 = Float.parseFloat(scanner.nextLine());
         scanner.close();
-        if (lado1 == lado2 && lado2 == lado3){
+        if (lado1 == lado2 && lado2 == lado3) {
             System.out.println("Triángulo equilátero");
-        }else if(){
-            
+        } else if (lado1 != lado2 && lado2 != lado3 && lado1 != lado3) {
+            System.out.println("Triangulo escaleno");
+        } else {
+            System.out.println("Triangulo isosceles");
         }
+
 
     }
 }
