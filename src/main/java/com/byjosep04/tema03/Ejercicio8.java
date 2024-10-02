3cadab924701738b95285ec78c4cbc946ffddce3
Josep Vicent Mestre Llobell <jmestrellobell@gmail.com>
2024-10-02 16:57:19 +0200

    Ejercicio8 terminado

---
+++ src/main/java/com/byjosep04/tema03/Ejercicio8.java
@@ -0,0 +1,21 @@
+package com.byjosep04.tema03;
+
+import java.util.Scanner;
+
+public class Ejercicio8 {
+    public static void main(String[] args) {
+        int dado;
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Ingrese el numero de la cara");
+        dado = Integer.parseInt(scanner.nextLine());
+        switch (dado) {
+            case 1 -> System.out.println(6);
+            case 2 -> System.out.println(5);
+            case 3 -> System.out.println(4);
+            case 4 -> System.out.println(3);
+            case 5 -> System.out.println(2);
+            case 6 -> System.out.println(1);
+            default -> System.err.println("ERROR: número incorrecto");
+        }
+    }
+}
