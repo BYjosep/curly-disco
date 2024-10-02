08d7adf766c79980814960de991b78a550023657
Josep Vicent Mestre Llobell <jmestrellobell@gmail.com>
2024-10-02 16:03:00 +0200

    Ejercicio5 correjido. Cambio case 6 a Bien y case 7 y 8 a notable

--- src/main/java/com/byjosep04/tema03/Ejercicio5.java
+++ src/main/java/com/byjosep04/tema03/Ejercicio5.java
@@ -13,12 +13,16 @@ public class Ejercicio5 {
             case 0: case 1: case 2: case 3: case 4:
                 System.out.println("INSUFICIENTE");
                 break;
-            case 5: case 6:
+            case 5:
                 System.out.println("SUFICIENTE");
                 break;
-            case 7: case 8:
+            case 6:
                 System.out.println("BIEN");
                 break;
+            case 7: case 8:
+                System.out.println("NOTABLE");
+                break;
+
             case 9: case 10:
                 System.out.println("EXELENTE");
                 break;
