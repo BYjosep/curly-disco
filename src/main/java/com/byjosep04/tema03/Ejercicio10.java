8d29dd302391e38698cf4fcbd6b18f33be11b998
Josep Vicent Mestre Llobell <jmestrellobell@gmail.com>
2024-10-03 02:18:37 +0200

    Ejercicio10 Terminado

---
+++ src/main/java/com/byjosep04/tema03/Ejercicio10.java
@@ -0,0 +1,24 @@
+package com.byjosep04.tema03;
+
+import java.util.Scanner;
+
+public class Ejercicio10 {
+    public static void main(String[] args) {
+        int anyo;
+
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Ingrese el año");
+        anyo = Integer.parseInt(scanner.nextLine());
+        scanner.close();
+        if (anyo % 400 == 0) {
+            System.out.println("El año es bisiesto");
+        } else {
+            if (anyo % 4 == 0 && anyo % 100 != 0) {
+                System.out.println("El año es bisiesto");
+
+            } else {
+                System.out.println("El año no es bisiesto");
+            }
+        }
+    }
+}
