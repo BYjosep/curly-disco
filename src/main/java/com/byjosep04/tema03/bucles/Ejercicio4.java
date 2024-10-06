c7594cb64817ddec95ba0b083b38223c31462bc1
Josep Vicent Mestre Llobell <jmestrellobell@gmail.com>
2024-10-06 17:16:45 +0200

    Ejercicio4 bucles Terminado

---
+++ src/main/java/com/byjosep04/tema03/bucles/Ejercicio4.java
@@ -0,0 +1,11 @@
+package com.byjosep04.tema03.bucles;
+
+public class Ejercicio4 {
+    public static void main(String[] args) {
+        int i;
+        for (i = 1; i <= 100; i++)
+            if (i % 2 == 1) {
+                System.out.println(i);
+            }
+    }
+}
