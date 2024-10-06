fa9e159b4ebe0565dfb095dc8819178f4cca60ec
Josep Vicent Mestre Llobell <jmestrellobell@gmail.com>
2024-10-06 13:12:12 +0200

    Ejercicio1 bucles Terminado

--- src/main/java/com/byjosep04/tema03/bucles/Ejercicio1.java
+++ src/main/java/com/byjosep04/tema03/bucles/Ejercicio1.java
@@ -1,4 +1,10 @@
 package com.byjosep04.tema03.bucles;
 
 public class Ejercicio1 {
+    public static void main(String[] args) {
+        int numero;
+        for (numero = 45; numero <= 100; numero++) {
+            System.out.println(numero);
+        }
+    }
 }
