a687f9db92865f47c25f1ca91b25b793ac082334
Josep Vicent Mestre Llobell <jmestrellobell@gmail.com>
2024-10-07 20:52:05 +0200

    Ejerciocio6 terminado

---
+++ src/main/java/com/byjosep04/tema03/bucles/Ejercicio6.java
@@ -0,0 +1,27 @@
+package com.byjosep04.tema03.bucles;
+
+import java.util.Scanner;
+
+public class Ejercicio6 {
+    public static void main(String[] args) {
+        int numeroEntrada, validate, multiplicacion;
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Introduzca un numero del 1-10");
+        do {
+            numeroEntrada = Integer.parseInt(scanner.nextLine());
+            if (numeroEntrada > 10 || numeroEntrada < 1) {
+                validate = 0;
+                System.out.println("Solo numeros del 1 al 10");
+            } else {
+                validate = 1;
+            }
+        } while (validate == 0);
+        scanner.close();
+        for (int i = 0; i <= 10; i++){
+            multiplicacion= i*numeroEntrada;
+            System.out.println(numeroEntrada+"X"+i+" es: "+ multiplicacion);
+        }
+
+
+    }
+}
