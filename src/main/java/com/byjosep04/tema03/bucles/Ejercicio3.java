c45521a43fd711bb052afb08605492d843fcd16a
Josep Vicent Mestre Llobell <jmestrellobell@gmail.com>
2024-10-07 20:27:18 +0200

    update Ejercicio3 y Ejercicio4.
    *En algun punto del 3 el nuevo codigo falla (muy posible este mal)

--- src/main/java/com/byjosep04/tema03/bucles/Ejercicio3.java
+++ src/main/java/com/byjosep04/tema03/bucles/Ejercicio3.java
@@ -5,6 +5,7 @@ import java.util.Scanner;
 public class Ejercicio3 {
     public static void main(String[] args) {
         int numero1, numero2, producto;
+        int signo1, signo2;
         Scanner scanner = new Scanner(System.in);
         System.out.println("Ingrese el primer numero");
         numero1 = Integer.parseInt(scanner.nextLine());
@@ -12,8 +13,7 @@ public class Ejercicio3 {
         numero2 = Integer.parseInt(scanner.next());
         scanner.close();
         producto = 0;
-        while (numero2 != 0) {
-
+        /*while (numero2 != 0) {
             if (numero2 < 0) {
                 ++numero2;
                 producto = producto + numero1;
@@ -24,8 +24,16 @@ public class Ejercicio3 {
             } else if (numero2 > 0) {
                 --numero2;
                 producto = producto + numero1;
-            }
+            }*/
+        //En algun punto del codigo falla
+        signo1 = numero1 < 0 ? -1 : 1;
+        signo2 = numero2 < 0 ? -1 : 1;
+        for (int i = 1; i <= numero1; i++) {
+            producto = producto + numero2;
         }
+        producto= producto*signo1*signo2;
+
+        //}
         System.out.println(producto);
     }
 }
--- src/main/java/com/byjosep04/tema03/bucles/Ejercicio4.java
+++ src/main/java/com/byjosep04/tema03/bucles/Ejercicio4.java
@@ -2,10 +2,13 @@ package com.byjosep04.tema03.bucles;
 
 public class Ejercicio4 {
     public static void main(String[] args) {
-        int i;
+        int i, impares;
+        impares=0;
         for (i = 1; i <= 100; i++)
             if (i % 2 == 1) {
                 System.out.println(i);
+                impares++;
             }
+        System.out.println("Hay "+impares+" impares");
     }
 }
