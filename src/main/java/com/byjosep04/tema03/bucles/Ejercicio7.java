257e5a7e4edb7b057a9950dc4ac5598f8983f497
Josep Vicent Mestre Llobell <jmestrellobell@gmail.com>
2024-10-07 20:58:44 +0200

    Ejerciocio7 hacer

---
+++ src/main/java/com/byjosep04/tema03/bucles/Ejercicio7.java
@@ -0,0 +1,12 @@
+package com.byjosep04.tema03.bucles;
+
+import java.util.Scanner;
+
+public class Ejercicio7 {
+    public static void main(String[] args) {
+        int a,b;
+        Scanner scanner =new Scanner(System.in);
+        System.out.println("Ingrese la base");
+        a= Integer.parseInt(scanner.nextLine());
+    }
+}
