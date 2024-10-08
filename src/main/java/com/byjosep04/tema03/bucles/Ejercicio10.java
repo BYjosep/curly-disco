f06b1b49d4b2aef1d31b030b35f5f15f0eaa8ee5
Josep Vicent Mestre Llobell <jmestrellobell@gmail.com>
2024-10-08 16:27:25 +0200

    Ejerciocio10 Terminado

---
+++ src/main/java/com/byjosep04/tema03/bucles/Ejercicio10.java
@@ -0,0 +1,18 @@
+package com.byjosep04.tema03.bucles;
+
+import java.util.Scanner;
+
+public class Ejercicio10 {
+    public static void main(String[] args) {
+        double numero,suma;
+        suma =0;
+        Scanner scanner= new Scanner(System.in);
+        do {
+            System.out.println("Ingrese un numero;");
+            numero = Double.parseDouble(scanner.nextLine());
+            suma+=numero;
+        }while (numero !=0);
+        scanner.close();
+        System.out.println(suma);
+    }
+}
