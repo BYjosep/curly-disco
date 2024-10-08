339dba40492a5040f27e916c0dbe666502c5915f
Josep Vicent Mestre Llobell <jmestrellobell@gmail.com>
2024-10-08 02:40:19 +0200

    Ejercicio9 update

--- src/main/java/com/byjosep04/tema03/bucles/Ejercicio9.java
+++ src/main/java/com/byjosep04/tema03/bucles/Ejercicio9.java
@@ -11,19 +11,16 @@ public class Ejercicio9 {
         numero = Integer.parseInt(scanner.nextLine());
         scanner.close();
         for (int i = numero; i > 0; i--) {
-            if (numero % i == 0 || 1 % i == 0) {
+            if (numero % i == 0) {
                 if (i != numero && 1 % i != 0) {
                     primario = 1;
                     System.out.println("El numero no es primo");
                     break;
                 }
-
             }
         }
         if (primario == 0) {
             System.out.println("El numero es primario");
         }
-
-
     }
 }
