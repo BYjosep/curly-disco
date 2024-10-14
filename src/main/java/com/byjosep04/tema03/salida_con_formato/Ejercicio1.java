d9be9d239adbbef8a4d56a24250987e137059920
Josep Vicent Mestre Llobell <jmestrellobell@gmail.com>
2024-10-15 00:12:57 +0200

    Ejercicio 1 terminado

--- src/main/java/com/byjosep04/tema03/salida_con_formato/Ejercicio1.java
+++ src/main/java/com/byjosep04/tema03/salida_con_formato/Ejercicio1.java
@@ -1,4 +1,17 @@
 package com.byjosep04.tema03.salida_con_formato;
 
+import java.util.Scanner;
+
 public class Ejercicio1 {
+    public static void main(String[] args) {
+        float numero1, numero2;
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Escriba el primer numero");
+        numero1 = Float.parseFloat(scanner.nextLine());
+        System.out.println("Escriba el segundo numero");
+        numero2 = Float.parseFloat(scanner.nextLine());
+        scanner.close();
+        System.out.printf("El primer numero es: %.2f %nEl segundo numero es: %.2f", numero1, numero2);
+
+    }
 }
