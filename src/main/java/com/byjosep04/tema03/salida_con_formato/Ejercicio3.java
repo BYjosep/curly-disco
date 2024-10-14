7e5e7ccad12a98a69057755a8b71ace469418e83
Josep Vicent Mestre Llobell <jmestrellobell@gmail.com>
2024-10-15 01:59:41 +0200

    Ejercicio 3 No se quiere alinear a la derecha

--- src/main/java/com/byjosep04/tema03/salida_con_formato/Ejercicio3.java
+++ src/main/java/com/byjosep04/tema03/salida_con_formato/Ejercicio3.java
@@ -1,4 +1,14 @@
 package com.byjosep04.tema03.salida_con_formato;
 
 public class Ejercicio3 {
+    public static void main(String[] args) {
+        int numeros = 1;
+        final int ESCALERA = 10;
+        for (int i = numeros; i < ESCALERA; i++) {
+            for (int recorido = numeros; recorido <= i; recorido++) {
+                System.out.printf("%d", recorido);
+            }
+            System.out.println();
+        }
+    }
 }
