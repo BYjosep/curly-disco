6fcf4da9f2c9633130571f87572664a5e6aa55ef
Josep Vicent Mestre Llobell <jmestrellobell@gmail.com>
2024-10-15 01:16:56 +0200

    Ejercicio 2 terminado

--- src/main/java/com/byjosep04/tema03/salida_con_formato/Ejercicio2.java
+++ src/main/java/com/byjosep04/tema03/salida_con_formato/Ejercicio2.java
@@ -1,4 +1,26 @@
 package com.byjosep04.tema03.salida_con_formato;
 
+import java.util.Scanner;
+
 public class Ejercicio2 {
+    public static void main(String[] args) {
+        int hora, minutos, segundos;
+        Scanner scanner = new Scanner(System.in);
+
+        do {
+            System.out.println("Ingrese la hora");
+            hora = Integer.parseInt(scanner.nextLine());
+        } while (hora > 23);
+        do {
+            System.out.println("Ingrese los minutos");
+            minutos = Integer.parseInt(scanner.nextLine());
+        } while (minutos > 59);
+
+        do {
+            System.out.println("Ingrese los segundos");
+            segundos = Integer.parseInt(scanner.nextLine());
+        } while (segundos > 59);
+
+        System.out.printf("La hora es: %02d:%02d:%02d", hora, minutos, segundos);
+    }
 }
