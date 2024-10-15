bdde721c1e4b50ba4d66f0b26c328a7d061d22ec
Josep Vicent Mestre Llobell <jmestrellobell@gmail.com>
2024-10-15 15:46:27 +0200

    Ejerciocio3 Correccion

--- src/main/java/com/byjosep04/tema03/salida_con_formato/Ejercicio3.java
+++ src/main/java/com/byjosep04/tema03/salida_con_formato/Ejercicio3.java
@@ -2,13 +2,12 @@ package com.byjosep04.tema03.salida_con_formato;
 
 public class Ejercicio3 {
     public static void main(String[] args) {
+        String cadena="";
         int numeros = 1;
         final int ESCALERA = 10;
         for (int i = numeros; i < ESCALERA; i++) {
-            for (int recorido = numeros; recorido <= i; recorido++) {
-                System.out.printf("%d", recorido);
-            }
-            System.out.println();
+            cadena=cadena + i;
+            System.out.printf("%9s\n",cadena);
         }
     }
 }
