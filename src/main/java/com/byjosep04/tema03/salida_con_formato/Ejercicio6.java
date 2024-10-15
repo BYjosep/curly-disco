34f36b984d8f629597e08bae7b1540bd2f61d1ee
Josep Vicent Mestre Llobell <jmestrellobell@gmail.com>
2024-10-15 16:28:45 +0200

    Ejerciocio6 Echo

---
+++ src/main/java/com/byjosep04/tema03/salida_con_formato/Ejercicio6.java
@@ -0,0 +1,14 @@
+package com.byjosep04.tema03.salida_con_formato;
+
+public class Ejercicio6 {
+    public static void main(String[] args) {
+        String cadena="";
+        int numeros = 1;
+        final int ESCALERA = 10;
+        for (int i = numeros; i < ESCALERA; i++) {
+            cadena = cadena + i;
+            System.out.printf("%9s", cadena);
+            System.out.printf("%s\n", cadena);
+        }
+    }
+}
