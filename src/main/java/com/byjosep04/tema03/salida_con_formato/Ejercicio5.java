e97f27867bf4ef940fcf8ae732a6a5f92102be31
Josep Vicent Mestre Llobell <jmestrellobell@gmail.com>
2024-10-15 16:25:43 +0200

    Ejerciocio5 Echo

---
+++ src/main/java/com/byjosep04/tema03/salida_con_formato/Ejercicio5.java
@@ -0,0 +1,21 @@
+package com.byjosep04.tema03.salida_con_formato;
+
+public class Ejercicio5 {
+    public static void main(String[] args) {
+        String cadena = "";
+        int numeros = 1;
+        final int ESCALERA = 10;
+        int c = 30;
+        int cMaximo = 37;
+        for (int i = numeros; i < ESCALERA; i++) {
+            cadena = cadena + i;
+            System.out.print("\u001B["+c+"m");
+            System.out.printf("%9s\n",cadena);
+            System.out.flush();
+            c++;
+            if (c > cMaximo) {
+                c = 30;
+            }
+        }
+    }
+}
