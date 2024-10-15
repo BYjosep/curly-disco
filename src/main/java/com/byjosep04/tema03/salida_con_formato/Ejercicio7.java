5e84d364305ea2a0b8572b6cc16aefbd8ff87070
Josep Vicent Mestre Llobell <jmestrellobell@gmail.com>
2024-10-15 17:09:53 +0200

    Ejercicio7 correccion. NO TERMINADO

---
+++ src/main/java/com/byjosep04/tema03/salida_con_formato/Ejercicio7.java
@@ -0,0 +1,32 @@
+package com.byjosep04.tema03.salida_con_formato;
+
+public class Ejercicio7 {
+    public static void main(String[] args) {
+        String cadena = "";
+        String cadena2="";
+        int recorido=8;
+        int numeros = 1;
+        final int ESCALERA = 10;
+        /*
+        Version 1
+        for (int i = numeros; i < ESCALERA; i++) {
+            cadena = cadena + i;
+            cadena2= i+cadena2;
+            System.out.printf("%9s%s\n", cadena,cadena2);
+
+        }*/
+
+        for (int i = 1; i < ESCALERA; i++) {
+            cadena = cadena + i;
+            System.out.printf("%9s\n", cadena);
+        }
+
+        for (int ib = 1; ib <ESCALERA ; ib++) {
+            System.out.print("\u001B["+recorido+"A");
+            cadena2 = ib + cadena2 ;
+            System.out.printf("%s\n", cadena2);
+            recorido --;
+        }
+
+    }
+}
---
+++ src/main/java/com/byjosep04/tema03/salida_con_formato/Ejercicio8.java
@@ -0,0 +1,7 @@
+package com.byjosep04.tema03.salida_con_formato;
+
+public class Ejercicio8 {
+    public static void main(String[] args) {
+        
+    }
+}
