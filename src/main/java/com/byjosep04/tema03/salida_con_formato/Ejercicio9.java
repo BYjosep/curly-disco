88868b669a93cf68a62497a7a8e94780fffb856c
Josep Vicent Mestre Llobell <jmestrellobell@gmail.com>
2024-10-16 17:16:07 +0200

    Ejercicio9 No Terminado

---
+++ src/main/java/com/byjosep04/tema03/salida_con_formato/Ejercicio9.java
@@ -0,0 +1,15 @@
+package com.byjosep04.tema03.salida_con_formato;
+
+import java.util.Scanner;
+
+public class Ejercicio9 {
+    public static void main(String[] args) {
+        String fecha;
+        int dia,mes,ano;
+        int hora, minutos,segundos;
+        float temperatura, PresionAtmosferica;
+        int lluvia, radiacionUV;
+
+    }
+}
+
