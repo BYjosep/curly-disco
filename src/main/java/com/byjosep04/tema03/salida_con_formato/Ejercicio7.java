242bf1d75239e7a1f4be0370f1415454a47fe0fe
Josep Vicent Mestre Llobell <jmestrellobell@gmail.com>
2024-10-16 16:13:18 +0200

    Ejercicio7 terminado correccion

--- src/main/java/com/byjosep04/tema03/salida_con_formato/Ejercicio7.java
+++ src/main/java/com/byjosep04/tema03/salida_con_formato/Ejercicio7.java
@@ -6,30 +6,27 @@ public class Ejercicio7 {
         String cadena2="";
         int recorido=8;
         int numeros = 1;
-        final int ESCALERA = 10;
+
 
         //Version 1
-        for (int i = numeros; i < ESCALERA; i++) {
+        /*for (int i = numeros; i < ESCALERA; i++) {
             cadena = cadena + i;
             cadena2= i+cadena2;
             System.out.printf("%9s%s\n", cadena,cadena2);
             for (int j = 0; j < 1; j++) {
 
             }
-        }
-        /*
-        for (int i = 1; i < ESCALERA; i++) {
+        }*/
+
+        for (int i = 1; i < 10; i++) {
             cadena = cadena + i;
-            System.out.printf("%9s\n", cadena);
+            System.out.printf("%9s", cadena);
+            for (int j = i-1; j >=0; j--) {
+                System.out.print(cadena.charAt(j));
+            }
+            System.out.println();
         }
 
-        for (int ib = 1; ib <ESCALERA ; ib++) {
-            System.out.print("\u001B["+recorido+"A");
-            cadena2 = ib + cadena2 ;
-            System.out.printf("%s", cadena2);
-            System.out.println();
-            recorido --;
-        }*/
 
     }
 }
