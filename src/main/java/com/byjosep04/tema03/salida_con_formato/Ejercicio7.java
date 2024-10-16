bae2aea9ad546a3355e4502f42b85097128f7627
Josep Vicent Mestre Llobell <jmestrellobell@gmail.com>
2024-10-16 15:46:47 +0200

    Ejercicio7 terminado

--- src/main/java/com/byjosep04/tema03/salida_con_formato/Ejercicio7.java
+++ src/main/java/com/byjosep04/tema03/salida_con_formato/Ejercicio7.java
@@ -7,15 +7,17 @@ public class Ejercicio7 {
         int recorido=8;
         int numeros = 1;
         final int ESCALERA = 10;
-        /*
-        Version 1
+
+        //Version 1
         for (int i = numeros; i < ESCALERA; i++) {
             cadena = cadena + i;
             cadena2= i+cadena2;
             System.out.printf("%9s%s\n", cadena,cadena2);
+            for (int j = 0; j < 1; j++) {
 
-        }*/
-
+            }
+        }
+        /*
         for (int i = 1; i < ESCALERA; i++) {
             cadena = cadena + i;
             System.out.printf("%9s\n", cadena);
@@ -24,9 +26,10 @@ public class Ejercicio7 {
         for (int ib = 1; ib <ESCALERA ; ib++) {
             System.out.print("\u001B["+recorido+"A");
             cadena2 = ib + cadena2 ;
-            System.out.printf("%s\n", cadena2);
+            System.out.printf("%s", cadena2);
+            System.out.println();
             recorido --;
-        }
+        }*/
 
     }
 }
