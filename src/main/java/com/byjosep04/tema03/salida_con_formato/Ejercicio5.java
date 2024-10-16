a249f6e72dae929595b803a0d6c3e4cc26909688
Josep Vicent Mestre Llobell <jmestrellobell@gmail.com>
2024-10-16 15:35:24 +0200

    Ejercicio5 correccion

--- src/main/java/com/byjosep04/tema03/salida_con_formato/Ejercicio5.java
+++ src/main/java/com/byjosep04/tema03/salida_con_formato/Ejercicio5.java
@@ -3,14 +3,13 @@ package com.byjosep04.tema03.salida_con_formato;
 public class Ejercicio5 {
     public static void main(String[] args) {
         String cadena = "";
-        int numeros = 1;
         final int ESCALERA = 10;
         int c = 30;
         int cMaximo = 37;
-        for (int i = numeros; i < ESCALERA; i++) {
+        for (int i = 1; i < ESCALERA; i++) {
             cadena = cadena + i;
             System.out.print("\u001B[" + c + "m");
-            System.out.printf("%9s\n\u001B[0m", cadena);
+            System.out.printf("%9s\n \u001B[0m", cadena);
             c++;
             if (c > cMaximo) {
                 c = 30;
